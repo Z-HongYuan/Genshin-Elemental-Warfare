d3427481347609157a8ccaefa6a47b5dbf6b60b7
Z-HongYuan <1939218189@qq.com>
2025-03-24 21:29:48 +0800

    初始Commit

---
+++ Source/GenshinElementalWar/GenshinElementalWar.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GenshinElementalWar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GenshinElementalWar, "GenshinElementalWar" );
