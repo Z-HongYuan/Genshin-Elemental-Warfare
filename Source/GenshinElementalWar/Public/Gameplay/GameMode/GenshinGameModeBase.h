37242aff6b53b5be06535002048f43c78c994a80
Z-HongYuan <1939218189@qq.com>
2025-03-24 22:00:38 +0800

    Add Plugin : KawaiiPhysics / AutoNodeArranger / AutoSizeComments / FlatNodes / SwitchLanguage / GameplayAbilities / AnimationWarping / AnimationLocomotionLibrary / ChaosVehiclesPlugin 创建GameModeBase

---
+++ Source/GenshinElementalWar/Public/Gameplay/GameMode/GenshinGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GenshinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHINELEMENTALWAR_API AGenshinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
