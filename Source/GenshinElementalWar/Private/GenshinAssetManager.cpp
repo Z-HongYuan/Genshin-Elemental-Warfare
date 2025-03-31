b4642edfa7ffcc42f74ff969629460077898c5b0
Z-HongYuan <1939218189@qq.com>
2025-03-31 22:42:03 +0800

    构建GameplayTag的代码添加逻辑

---
+++ Source/GenshinElementalWar/Private/GenshinAssetManager.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+
+#include "GenshinAssetManager.h"
+
+#include "GenshinGameplayTags.h"
+
+UGenshinAssetManager& UGenshinAssetManager::Get()
+{
+	check(GEngine);
+	
+	return *Cast<UGenshinAssetManager>(GEngine->AssetManager);
+}
+
+void UGenshinAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	
+	FGenshinGameplayTags::InitializeNativeGameplayTags();
+}
---
+++ Source/GenshinElementalWar/Public/GenshinAssetManager.h
@@ -0,0 +1,26 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "GenshinAssetManager.generated.h"
+
+/**
+ * 资产管理器
+ * 单例,全局唯一
+ * 
+ */
+UCLASS()
+class GENSHINELEMENTALWAR_API UGenshinAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+public:
+	static UGenshinAssetManager& Get();
+
+protected:
+
+	//注册资产,Tag资产
+	virtual void StartInitialLoading() override;
+};
