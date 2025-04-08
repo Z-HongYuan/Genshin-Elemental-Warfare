4d83b4294d7bc30f807aeaa04c56eeaf27bc2726
Z-HongYuan <1939218189@qq.com>
2025-04-08 21:55:28 +0800

    添加抛射物Actor 修改项目设置

---
+++ Source/GenshinElementalWar/Public/Actors/GenshinProjectileActor.h
@@ -0,0 +1,36 @@
+﻿// Copyright © 2025 鸿源z 保留所有权利
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "GenshinProjectileActor.generated.h"
+
+UCLASS()
+class GENSHINELEMENTALWAR_API AGenshinProjectileActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGenshinProjectileActor();
+
+	//TODO 应用GE到碰撞的Pawn
+	UFUNCTION(BlueprintCallable, Category = "Projectile")
+	void ApplyEffectToOverlap(AActor* ApplyActor,bool bIgnorePlayer);
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileMesh")
+	TObjectPtr<UStaticMeshComponent> ProjectileMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CapsuleComponent")
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ProjectileMovementComponent")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	
+public:
+	virtual void Tick(float DeltaTime) override;
+};
